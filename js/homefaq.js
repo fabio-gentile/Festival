4a106f51e0a4b9ee1f491f7bbaf908ba989af80f
Fabio Gentile <gentilef.fabio@gmail.com>
2023-06-07 17:56:43 +0200

    ticket

---
+++ js/homefaq.js
@@ -0,0 +1,10 @@
+const questions = document.querySelectorAll('.home-faq .wrapper .question')
+
+questions.forEach((element, index) => {
+    element.addEventListener('click', () => {
+        element.classList.toggle('active')
+        const title = element.querySelector('.title');
+        title.scrollIntoView({ behavior: 'auto', block: 'center' });
+    })
+})
+
